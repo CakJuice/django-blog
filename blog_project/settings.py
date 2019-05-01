5e14d25287a02ed706da8cb41fbe83dd54cbb54d
Hendra juice <hd.brandoz@gmail.com>
2019-05-01 08:15:40 +0700

    add local settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+try:
+    import local_settings
+except Exception:
+    raise Exception("Local settings not found!")
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,17 +79,7 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'blog_db',
-        'PORT': 5432,
-    }
+    'default': local_settings.PG_SETUP
 }
 
 
