6afb59622a32b5d478c99e76788ac45b887cf99f
Hendra juice <hd.brandoz@gmail.com>
2019-05-01 09:14:31 +0700

    create base view for category

--- blog_project/post/views.py
+++ blog_project/post/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from .models import Category
+
 
 # Create your views here.
+class CategoryListView(ListView):
+    model = Category
+    paginate_by = 20
+    template_name = 'category/index.html'
