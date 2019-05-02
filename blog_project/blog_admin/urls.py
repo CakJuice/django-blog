b0d688d60ec3bf767815019cf47089f8fd8b116a
Hendra juice <hd.brandoz@gmail.com>
2019-05-02 10:00:42 +0700

    prepare base admin template

--- blog_project/blog_admin/urls.py
+++ blog_project/blog_admin/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='admin_index'),
     path('dashboard/', views.dashboard, name='admin_dashboard'),
 ]
--- blog_project/blog_admin/views.py
+++ blog_project/blog_admin/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+def index(request):
+    return redirect('admin_dashboard')
+
+
+@login_required
 def dashboard(request):
     return render(request, 'admin/dashboard.html')
