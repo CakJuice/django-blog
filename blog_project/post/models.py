3546a387970b77a17e7188a64712867bac99591d
Hendra juice <hd.brandoz@gmail.com>
2019-05-03 19:18:43 +0700

    prepare i18n

--- blog_project/post/models.py
+++ blog_project/post/models.py
@@ -2,25 +2,26 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created At"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated At"))
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, related_name='+',
-                                   verbose_name="Created By")
+                                   verbose_name=_("Created By"))
 
     class Meta:
         abstract = True
 
 
 class Category(BaseModel):
-    name = models.CharField(max_length=60, verbose_name="Name")
-    description = models.CharField(max_length=158, verbose_name="Description", blank=True)
-    slug = models.CharField(max_length=64, unique=True, db_index=True, verbose_name="Slug", blank=True)
+    name = models.CharField(max_length=60, verbose_name=_("Name"))
+    description = models.CharField(max_length=158, verbose_name=_("Description"), blank=True)
+    slug = models.CharField(max_length=64, unique=True, db_index=True, verbose_name=_("Slug"), blank=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='childs',
-                               verbose_name="Parent")
+                               verbose_name=_("Parent Category"))
 
     class Meta:
         db_table = settings.DB_TABLE_PREFIX + 'category'
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,3 +153,8 @@ try:
     EMAIL_USE_SSL = True
 except ValueError:
     print("[WARNING] Local settings for email not found, your application may not work properly!")
+
+# locale path
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
